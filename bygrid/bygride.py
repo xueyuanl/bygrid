47e04a97059ab4d79b0bff4face2504ce04c7830
macbook <15186846+xueyuanl@users.noreply.github.com>
2021-08-20 16:05:16 +0100

    add put order demo

--- bygrid/bygride.py
+++ bygrid/bygride.py
@@ -1,5 +1,31 @@
+import json
+import os
+
+from binance.spot import Spot
+
+
 def main():
-    pass
+    client = Spot()
+    print(client.time())
+
+    client = Spot(key=os.environ['KEY'], secret=os.environ['SECRET'])
+
+    # Get account information
+    print(json.dumps(client.account()))
+
+    # Post a new order
+    params = {
+        'symbol': 'DOGEUSDT',
+        'side': 'BUY',
+        'type': 'LIMIT',
+        'timeInForce': 'GTC',
+        'quantity': 100,
+        'price': 0.2
+    }
+
+    response = client.new_order(**params)
+    #response = client.coin_info()
+    print(json.dumps(response))
 
 
 if __name__ == '__main__':
